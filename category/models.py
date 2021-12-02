8574d548bca46d1295153624e22d687d572ba1d0
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-12-01 19:46:31 -0600

    Categorias y slug en navbar
    Se filtran las categorias de la BDD y se muestran en el navbar.
    Estas categorias al dar clic agregan su slug al url automaticamente

--- category/models.py
+++ category/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
@@ -11,6 +12,9 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
         
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+    
     def __str__(self):
         return self.category_name
     
