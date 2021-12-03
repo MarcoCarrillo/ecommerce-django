e196f7790c62721c789595b1c1ea745b55536289
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-12-02 18:45:58 -0600

    Trayendo la data del la DB al shopping cart

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 from .models import Cart, CartItem
 
@@ -28,5 +29,20 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
                 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass #Ignora la excepcion
+    
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }   
+    
+    return render(request, 'store/cart.html', context)
