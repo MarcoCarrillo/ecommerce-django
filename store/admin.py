306ceaae7f353b371512a33d05f4515fab0c8e93
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-12-07 16:34:36 -0600

    Variations en el carrito de compras

--- store/admin.py
+++ store/admin.py
@@ -7,9 +7,9 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
     
 class VariationAdmin(admin.ModelAdmin):
-    list_display = ('product', 'variation_category', 'variation_value', 'is_active',)
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
     list_editable = ('is_active',)
-    list_filter = ('product', 'variation_category', 'variation_value', 'is_active',)    
+    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')    
     
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
--- store/models.py
+++ store/models.py
@@ -32,18 +32,18 @@ class VariationManager(models.Manager):
 
 variation_category_choice = (
     ('color', 'color'),
-    ('talla', 'talla')
+    ('talla', 'talla'),
 )
 
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    variation_category = models.CharField(max_length=100, choices= variation_category_choice)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
     variation_value = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True) 
     
     objects = VariationManager()
     
-    def __unicode__(self):
-        return self.product   
+    def __str__(self):
+        return self.variation_category + ':' + self.variation_value   
     
